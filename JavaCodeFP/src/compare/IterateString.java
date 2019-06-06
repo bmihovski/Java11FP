45d2eb95d057b41e5cd21a6ac4c86c48514deb2b
bmihovski <bmihovski@live.com>
2019-06-06 23:41:48 +0300

    Compare examples including objects manipulations

--- JavaCodeFP/src/compare/IterateString.java
+++ JavaCodeFP/src/compare/IterateString.java
@@ -1,12 +1,24 @@
 package compare;
 
 public class IterateString {
+	
+	private static void printChar(int aChar) {
+		System.out.println((char) (aChar));
+	}
 
 	public static void main(String[] args) {
 		final String str = "w00t";
 		
 		str.chars()
+		   .forEach(IterateString::printChar);
+		
+		str.chars()
+		   .mapToObj(ch -> Character.valueOf((char)ch))
 		   .forEach(System.out::println);
+		
+		str.chars()
+		   .filter(Character::isDigit)
+		   .forEach(IterateString::printChar);
 
 	}
 
