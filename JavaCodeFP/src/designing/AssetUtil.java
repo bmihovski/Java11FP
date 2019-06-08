d006e1c8376fb0ae50ae3faa2987842d4152de8d
bmihovski <bmihovski@live.com>
2019-06-08 17:08:09 +0300

    Strategy pattern

---
+++ JavaCodeFP/src/designing/AssetUtil.java
@@ -0,0 +1,36 @@
+package designing;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import designing.Asset.AssetType;
+
+public class AssetUtil {
+	
+	public static int totalAssetValues(final List<Asset> assets,
+			Predicate<Asset> assetSelector) {
+		return assets.stream()
+					 .filter(assetSelector)
+					 .mapToInt(Asset::getValue)
+					 .sum();
+	}
+	
+
+	public static void main(String[] args) {
+		final List<Asset> assets = Arrays.asList(
+				new Asset(AssetType.BOND, 1000),
+				new Asset(AssetType.BOND, 2000),
+				new Asset(AssetType.STOCK, 3000),
+				new Asset(AssetType.STOCK, 4000)
+				);
+		
+		System.out.println("Total of all assets: " + totalAssetValues(assets, asset -> true));
+		
+		System.out.println("Total of bonds: " + totalAssetValues(assets, asset -> asset.getType() == AssetType.BOND));
+		
+		System.out.println("Total of stocks: " + totalAssetValues(assets, asset -> asset.getType() == AssetType.STOCK));
+
+	}
+
+}
