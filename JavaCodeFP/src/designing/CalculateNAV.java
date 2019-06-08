4177c322878f1626361932080649262acbb694b6
bmihovski <bmihovski@live.com>
2019-06-08 18:02:52 +0300

    Call webservice with test

---
+++ JavaCodeFP/src/designing/CalculateNAV.java
@@ -0,0 +1,31 @@
+package designing;
+
+import java.math.BigDecimal;
+import java.util.function.Function;
+
+public class CalculateNAV {
+	
+	public BigDecimal computeStockWorth(
+			final String ticker, final int shares) {
+		return priceFinder.apply(ticker).multiply(BigDecimal.
+				valueOf(shares));
+	}
+	
+	private Function<String, BigDecimal> priceFinder;
+	
+	public CalculateNAV(final Function<String,
+			BigDecimal> aPriceFinder) {
+		priceFinder = aPriceFinder;
+	}
+
+	public static void main(String[] args) {
+		final CalculateNAV calculateNAV =
+				new CalculateNAV(YahooFinance::getPrice);
+		
+		System.out.println(String.format("100 shares of "
+				+ "Google worth: $%.2f",
+				calculateNAV.computeStockWorth("GOOG", 100)));
+
+	}
+
+}
