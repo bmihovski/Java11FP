266730b65004984fbc4748042fa1906bf61bf4b8
Gogs <gogs@fake.local>
2019-06-10 10:42:57 +0300

    Junit 4 replacing Junit 5 to avoid conflicts

--- JavaCodeFP/src/designing/CalculateNAVTest.java
+++ JavaCodeFP/src/designing/CalculateNAVTest.java
@@ -1,10 +1,10 @@
 package designing;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class CalculateNAVTest {
 
